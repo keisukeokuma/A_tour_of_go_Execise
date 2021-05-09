210f6820e812738079ee509292cfccb08bbac23b
Keisuke Okuma <k_okuma@cross-c.co.jp>
2021-05-09 21:51:32 +0900

    コメントの追加

--- Concurrency/05select.go
+++ Concurrency/05select.go
@@ -1,15 +1,18 @@
 package main
 
+// fmtパッケージを使用するためにImportする
 import "fmt"
 
+// fibonacci関数の定義 int型のチャネル c を引数としてとる
 func fibonacci(c, quit chan int) {
 	x, y := 0, 1
 	for {
+		// selectで囲むことで準備のできたcaseから実行する
 		select {
 		// ➂xの値をcに送信する。初期値は0。その後x, y = y, x+yの値を加える。
 		case c <- x:
 			x, y = y, x+y
-		// ⓺quitを受信できる場合はquitを出力して終了する。
+		// ⑥quitを受信できる場合はquitを出力して終了する。
 		case <-quit:
 			fmt.Println("quit")
 			return
@@ -18,6 +21,7 @@ func fibonacci(c, quit chan int) {
 }
 
 func main() {
+	// 変数定義 int型チャネル c,quit
 	c := make(chan int)
 	quit := make(chan int)
 	// 非同期通信のイメージ(ajaxのweb側)
@@ -27,7 +31,7 @@ func main() {
 			// ➃<-cで受信した値を出力する。
 			fmt.Println(<-c)
 		}
-		// ⓹➃が10回繰り返されたらquitに0を送信する。
+		// ⑤➃が10回繰り返されたらquitに0を送信する。
 		quit <- 0
 	}()
 	// ➁関数呼び出し
