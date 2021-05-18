2c65b621e571b4f79ba1d16874777aa39af281fd
Keisuke Okuma <k_okuma@cross-c.co.jp>
2021-05-18 18:07:51 +0900

    レビュー内容の修正

--- Methods_and_interfaces/22Readers.go
+++ Methods_and_interfaces/22Readers.go
@@ -7,7 +7,7 @@ import (
 // 構造体MyReaderを定義
 type MyReader struct{}
 
-// Readメソッドを定義する。値レシーバとしてMyReaderを受け取る。
+// Readメソッドを定義する。値レシーバとしてrを受け取る。
 // また、引数を[]byte型のbを取り、返り値をint型とerrorインターフェイスを満たす値を返す。
 func (r MyReader) Read(b []byte) (int, error) {
 	// 繰り返し処理
@@ -21,7 +21,7 @@ func (r MyReader) Read(b []byte) (int, error) {
 func main() {
 	/*
 	 * ValidateにReadメソッドを持つ構造体MyReader{}を引数として渡す。
-	 * Validate内で'A'が繰り返し作成されているかをチャックし、成功であれば"OK"を出力する
+	 * Validate内で'A'が繰り返し作成されているかをチェックし、成功であれば"OK"を出力する
 	 */
 	reader.Validate(MyReader{})
 }
