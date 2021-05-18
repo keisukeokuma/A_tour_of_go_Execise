31b2792edb57b39cff82df2992c2a4d1f4422989
Keisuke Okuma <k_okuma@cross-c.co.jp>
2021-05-18 11:01:20 +0900

    Methods and interfaces22  Readersについて

--- Methods_and_interfaces/22Readers.go
+++ Methods_and_interfaces/22Readers.go
@@ -1,18 +1,27 @@
 package main
 
-import "golang.org/x/tour/reader"
+import (
+	"golang.org/x/tour/reader"
+)
 
+// 構造体MyReaderを定義
 type MyReader struct{}
 
-// TODO: Add a Read([]byte) (int, error) method to MyReader.
-// (構造体)　read (引数)(返り値)
+// Readメソッドを定義する。値レシーバとしてMyReaderを受け取る。
+// また、引数を[]byte型のbを取り、返り値をint型とerrorインターフェイスを満たす値を返す。
 func (r MyReader) Read(b []byte) (int, error) {
+	// 繰り返し処理
 	for i := range b {
 		b[i] = 'A'
 	}
+	// len(b) bの長さを取得
 	return len(b), nil
 }
 
 func main() {
+	/*
+	 * ValidateにReadメソッドを持つ構造体MyReader{}を引数として渡す。
+	 * Validate内で'A'が繰り返し作成されているかをチャックし、成功であれば"OK"を出力する
+	 */
 	reader.Validate(MyReader{})
 }
