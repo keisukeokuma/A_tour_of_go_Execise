2052eae57838137ff2b7a9c4c87a27eb611a1787
Keisuke Okuma <k_okuma@cross-c.co.jp>
2021-05-18 18:16:54 +0900

    レビュー内容の修正　微調整

--- Methods_and_interfaces/22Readers.go
+++ Methods_and_interfaces/22Readers.go
@@ -7,7 +7,7 @@ import (
 // 構造体MyReaderを定義
 type MyReader struct{}
 
-// Readメソッドを定義する。値レシーバとしてrを受け取る。
+// Readメソッドを定義する。値レシーバとしてMyReader型のrを受け取る。
 // また、引数を[]byte型のbを取り、返り値をint型とerrorインターフェイスを満たす値を返す。
 func (r MyReader) Read(b []byte) (int, error) {
 	// 繰り返し処理
